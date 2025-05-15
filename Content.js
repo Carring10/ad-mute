db85a7ca9496ca8db17c6e17bc86d5de46770bec
Carrington <hartis.c99@gmail.com>
2025-05-15 17:26:11 -0400

    successfully detects ad and mutes tab

--- Content.js
+++ Content.js
@@ -1,18 +1,20 @@
 function checkForAdAndNotify() {
-  const isAd = document.querySelector('.CoreText-sc-1txzju1-0 ckwzla') !== null;
+  const isAd = document.querySelector('.CoreText-sc-1txzju1-0.ckwzla') !== null;
+
   console.log("Ad?", isAd);
+  console.log(document.querySelector('.CoreText-sc-1txzju1-0.ckwzla'))
 
   chrome.runtime.sendMessage({ action: "setMute", mute: isAd }, (response) => {
     console.log(`Mute toggle sent. Ad is ${isAd}. Response:`, response);
   });
 }
 
+const container = document.querySelector('.Layout-sc-1xcs6mc-0.SugpE') || document.body;
 const observer = new MutationObserver(() => {
   checkForAdAndNotify();
 });
 
-// Start observing the body class list
-observer.observe(document.body, {
-  attributes: true,
-  attributeFilter: ['class']
+observer.observe(container, {
+  childList: true,
+  subtree: true
 });
